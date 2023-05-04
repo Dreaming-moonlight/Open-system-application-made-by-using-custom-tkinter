85f48457cc479202b86b25d85be8f0fdf49a5f26
Dreaming-moonlight <132428434+Dreaming-moonlight@users.noreply.github.com>
2023-05-04 11:32:50 +0530

    Update

--- main.py
+++ main.py
@@ -4,10 +4,53 @@ import tkinter.messagebox as tkmb
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
 
-print("If you are a admin login here")
+admin_username = ["admin1", "admin2", "admin3", "admin4"]
+admin_password = ["development_process"]
+
+print("#If you are a admin login here, if you are just a user then double enter click")
+print("#After registration enter data in the list given in the code in line 13 and 14")
+
 username_admin = input("Enter admin username: ")
 userpass_admin = input("Enter admin password: ")
 
+users = ["Tejesh"]
+password = ['1234']
+
+if username_admin in admin_username and userpass_admin in admin_password:
+
+    new_page = ctk.CTk()
+
+    new_page.title("Logged in as admin")
+
+    new_page.geometry("500x500")
+
+    frame_admin = ctk.CTkFrame(master=new_page)
+    frame_admin.grid(pady=20, padx=40)
+
+    exit_button = ctk.CTkButton(frame_admin, text="Exit", command=lambda:{exit()})
+    exit_button.grid(pady=20, padx=10)
+
+    new_page.mainloop()
+
+elif username_admin == "" and userpass_admin == "":
+    print("Login screen opening....")
+
+elif username_admin not in admin_username and userpass_admin in admin_password:
+    print("Error", "Username is invalid")
+    print("Login page will also pop up in case you are a user")
+    print("Re-run code if you think you entered username wrong")
+
+elif username_admin in admin_username and userpass_admin not in admin_password:
+    print("Error", "Password in entered incorrectly")
+    print("Login page will also pop up in case you are a user")
+    print("Re-run code if you think you entered password wrong")
+
+elif username_admin not in admin_username and userpass_admin not in admin_password:
+    print("Error", "Username is invalid and password in incorrect")
+    print("Login page will also pop up in case you are a user")
+    print("Re-run code if you think you entered username and password wrong")
+
+print("Check taskbar")
 
 def register():
 
@@ -15,18 +58,22 @@ def register():
 
         def write_review():
 
-            def submit():
-                final = ctk.CTkToplevel(review_page)
+            def check():
+                if Label_entry.get() == "":
+                    tkmb.showerror("Error", "Review is blank")
 
-                final.title("Submitted")
+                else:
+                    last_page = ctk.CTkToplevel(review_page)
 
-                final.geometry("500x500")
+                    last_page.title("Submitted")
 
-                frame6 = ctk.CTkFrame(master=final)
-                frame6.grid(pady=20, padx=40)
+                    last_page.geometry("500x500")
 
-                label_message = ctk.CTkLabel(frame6, text="Sent review to the owner")
-                label_message.grid(pady=20, padx=10)
+                    frame6 = ctk.CTkFrame(master=last_page)
+                    frame6.grid(pady=20, padx=40)
+
+                    Label = ctk.CTkLabel(frame6, text="Submitted successfully")
+                    Label.grid(pady=20, padx=10)
 
             review_page = ctk.CTkToplevel(reg_page)
 
@@ -43,12 +90,15 @@ def register():
             Label_entry = ctk.CTkEntry(frame5, placeholder_text="Write review")
             Label_entry.grid(pady=20, padx=10)
 
-            submit_btn_2 = ctk.CTkButton(review_page, text="Send", command=submit)
+            submit_btn_2 = ctk.CTkButton(review_page, text="Send", command=check)
             submit_btn_2.grid(pady=20, padx=40)
+
+            cancel_button = ctk.CTkButton(review_page, text="Cancel", command=lambda:{exit()})
+            cancel_button.grid(pady=20, padx=40)
 
         reg_page = ctk.CTkToplevel(register_page)
 
-        reg_page.title("Write review")
+        reg_page.title("Registered successfully")
 
         reg_page.geometry("500x500")
 
@@ -72,6 +122,9 @@ def register():
 
         Review_button = ctk.CTkButton(frame4, text="Write a review", command=write_review)
         Review_button.grid(pady=20, padx=10)
+
+        cancel_button_2 = ctk.CTkButton(frame4, text="Cancel", command=lambda:{exit()})
+        cancel_button_2.grid(pady=20, padx=10)
 
 
     register_page = ctk.CTkToplevel(window)
@@ -104,7 +157,6 @@ def register():
     register_btn = ctk.CTkButton(frame3, text="Register", command=reg)
     register_btn.grid(pady=20, padx=10)
 
-
 def submit():
     submit_page = ctk.CTkToplevel(window)
 
@@ -112,7 +164,7 @@ def submit():
 
     submit_page.geometry("500x500")
 
-    if login_entry.get() == username_admin and password_entry.get() == userpass_admin and conform_password.get() == userpass_admin:
+    if login_entry.get() == username_admin or users and password_entry.get() == userpass_admin or password and conform_password.get() == userpass_admin or password:
         tkmb.showinfo(title="Login granted", message="You are authorized")
         label_submit_page = ctk.CTkLabel(submit_page, text="LOGGED IN")
         label_submit_page.grid(pady=20, padx=10)
